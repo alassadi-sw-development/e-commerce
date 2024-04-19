4c8c5b4a05578370a42440f6310febbb73dce836
Khalid <al-assadi_sw_development@hotmail.com>
2024-04-19 22:58:52 +0200

    Move addToCard Function into cart.js

--- scripts/data/cart.js
+++ scripts/data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let matchingCartItem;
+
+    cart.forEach((Cartitem)=>{
+        if (productId === Cartitem.productId){
+            matchingCartItem = Cartitem;
+        }
+    });
+    if (matchingCartItem){
+        matchingCartItem.quantity +=1;
+    }else {
+    cart.push({
+        productId: productId,
+        quantity: 1
+    });            
+    }
+}
