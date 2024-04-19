4608a7e40ba9d2e88c87839793027d85ba3daf5f
Khalid <al-assadi_sw_development@hotmail.com>
2024-04-20 00:40:30 +0200

    fix radio selectors

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 "user strict"
 import {cart, addToCart} from './data/cart.js'
 import {products} from './data/products.js'
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
@@ -18,7 +19,7 @@ products.forEach((product)=>{
         <div class="product-rating-count link-primary">${product.rating.count}</div>
     </div>
     <div class="product-price">
-        €${(product.priceCents/100).toFixed(2)}
+        €${formatCurrency(product.priceCents)}
     </div>
 
     <div class="product-quantity-container">
